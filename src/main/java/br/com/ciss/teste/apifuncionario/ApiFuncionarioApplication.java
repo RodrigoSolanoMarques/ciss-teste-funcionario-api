9dc5ae02208ad5dded2374ca1e7bdc1eb8303c93
Rodrigo Solano Marques <>
2021-07-25 17:34:55 -0300

    v0.0.1 - Criar projeto

---
+++ src/main/java/br/com/ciss/teste/apifuncionario/ApiFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.ciss.teste.apifuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFuncionarioApplication.class, args);
+	}
+
+}
